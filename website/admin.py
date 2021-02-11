c702ff09295e807baea122fd3a94e5d00f83140f
artem dotsenko <artemdotsenkousa@gmail.com>
2021-02-11 15:40:27 +0700

    New model

--- website/admin.py
+++ website/admin.py
@@ -10,10 +10,16 @@ class OtoplenieAdmin(admin.ModelAdmin):
 
 
 class AdressAdmin(admin.ModelAdmin):
-    list_display = ('name', 'organization')
+    list_display = ('name', 'main_adress')
     # readonly_fields = ('organization',)
+
+
+class MainAdressAdmin(admin.ModelAdmin):
+    list_display = ('name', 'organization')
+    list_filter = ('organization',)
 
 
 admin.site.register(Organization)
 admin.site.register(Adress, AdressAdmin)
 admin.site.register(Otoplenie, OtoplenieAdmin)
+admin.site.register(MainAdress, MainAdressAdmin)
--- website/models.py
+++ website/models.py
@@ -12,9 +12,22 @@ class Organization(models.Model):
         verbose_name_plural = "Организации"
 
 
+class MainAdress(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Главный адрес")
+    organization = models.ForeignKey('Organization', on_delete=models.PROTECT, null=True, verbose_name="Организация")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Главный адрес"
+        verbose_name_plural = "Главные адреса"
+
+
 class Adress(models.Model):
     name = models.CharField(max_length=150, verbose_name="Адрес")
-    organization = models.ForeignKey('Organization', on_delete=models.PROTECT, null=False, verbose_name="Организация")
+    main_adress = models.ForeignKey('MainAdress', on_delete=models.PROTECT, null=True, verbose_name="Главный адрес")
+    organization = models.ForeignKey('Organization', on_delete=models.PROTECT, null=True, verbose_name="Организация")
 
     def __str__(self):
         return self.name
@@ -22,13 +35,6 @@ class Adress(models.Model):
     class Meta:
         verbose_name = "Адрес"
         verbose_name_plural = "Адреса"
-
-
-class CalcManager(models.Manager): # Наш менеджер, который нам дает возможность менять поведение модели
-    def get_query_set(self):
-        result=super(CalcManager, self).get_query_set().extra(select={'total': "first+second"})
-        #то место где надо задавать алгоритм по которому вычисляется поле total
-        return result
 
 
 class Otoplenie(models.Model):
@@ -39,65 +45,9 @@ class Otoplenie(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Опубликовано")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
 
-    total = CalcManager()
-    objects = models.Manager()
-
     def __str__(self):
         return str(self.adress)
 
     class Meta:
         verbose_name = "Отопление"
         verbose_name_plural = "Отопление"
-
-
-# class Otoplenie(models.Model):
-#     adress = models.ForeignKey('Adress', on_delete=models.PROTECT, null=False, verbose_name="Адрес")
-#     organization = models.ForeignKey('Organization', on_delete=models.PROTECT, null=True, verbose_name="Организация")
-#     fact_jan = models.FloatField(default=0, verbose_name='Факт - январь')
-#     limit_jan = models.FloatField(default=0, verbose_name='Лимит - январь') # Временно
-#
-#     fact_feb = models.FloatField(default=0, verbose_name='Факт - февраль')
-#     limit_feb = models.FloatField(default=0, verbose_name='Лимит - февраль')
-#
-#     fact_mar = models.FloatField(default=0, verbose_name='Факт - март')
-#     limit_mar = models.FloatField(default=0, verbose_name='Лимит - март')
-#
-#     fact_apr = models.FloatField(default=0, verbose_name='Факт - апрель')
-#     limit_apr = models.FloatField(default=0, verbose_name='Лимит - апрель')
-#
-#     fact_may = models.FloatField(default=0, verbose_name='Факт - май')
-#     limit_may = models.FloatField(default=0, verbose_name='Лимит - май')
-#
-#     fact_jun = models.FloatField(default=0, verbose_name='Факт - июнь')
-#     limit_jun = models.FloatField(default=0, verbose_name='Лимит - июнь')
-#
-#     fact_jul = models.FloatField(default=0, verbose_name='Факт - июль')
-#     limit_jul = models.FloatField(default=0, verbose_name='Лимит - июль')
-#
-#     fact_aug = models.FloatField(default=0, verbose_name='Факт - август')
-#     limit_aug = models.FloatField(default=0, verbose_name='Лимит - август')
-#
-#     fact_sep = models.FloatField(default=0, verbose_name='Факт - сентябрь')
-#     limit_sep = models.FloatField(default=0, verbose_name='Лимит - сентябрь')
-#
-#     fact_oct = models.FloatField(default=0, verbose_name='Факт - октябрь')
-#     limit_oct = models.FloatField(default=0, verbose_name='Лимит - октябрь')
-#
-#     fact_nov = models.FloatField(default=0, verbose_name='Факт - ноябрь')
-#     limit_nov = models.FloatField(default=0, verbose_name='Лимит - ноябрь')
-#
-#     fact_dec = models.FloatField(default=0, verbose_name='Факт - декабрь')
-#     limit_dec = models.FloatField(default=0, verbose_name='Лимит - декабрь')
-#
-#     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Опубликовано")
-#     updated_at = models.DateTimeField(auto_now=True, verbose_name="Обновлено")
-#
-#     total = CalcManager()
-#     objects = models.Manager()
-#
-#     def __str__(self):
-#         return str(self.adress)
-#
-#     class Meta:
-#         verbose_name = "Отопление"
-#         verbose_name_plural = "Отопление"
